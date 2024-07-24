196aff521ce5b51ebd66cd5c8cb32e1411426d04
Shinyepo <shinyepo42@gmail.com>
2024-07-24 17:37:59 +0200

    package update

--- my-portfolio/src/index.tsx
+++ my-portfolio/src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { ColorModeScript } from "@chakra-ui/react";
 import { App } from "./App";
 import { theme } from "./theme";
-import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
@@ -21,8 +20,3 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
